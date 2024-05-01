ac98684e443992159316e7150484f988047460e1
Joel Kuruvilla <jl567056@dal.ca>
2024-05-01 14:51:56 -0300

    Completed Setup of Custom Provider Option

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,5 +1,8 @@
-import GitHubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/app/(models)/User";
+import bcrypt from "bcrypt";
 
 export const options = {
     providers: [
@@ -33,7 +36,49 @@ export const options = {
             },
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_Secert,
-        })
+        }),
+        // This is a Custom Provider!
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "email",
+                    type: "text",
+                    placeholder: "your-email",
+                },
+                password: {
+                    label: "password",
+                    type: "password",
+                    placeholder: "your-password",
+                },
+            },
+            async authorize(credentials){
+                try {
+                    const foundUser = await User.findOne({email: credentials.email})
+                    .lean()
+                    .exec();
+
+                    if(foundUser){
+                        console.log("User Exists"); //Remove after testing phase, not for deployment?
+                        const match = await bcrypt.compare(
+                            credentials.password,
+                            foundUser.password
+                        );
+
+                        if (match){
+                            console.log("Good Pass"); //Remove after testing phase, not for deployment!
+                            delete foundUser.password;
+
+                            foundUser["role"] = "Unverified Email";
+                            return foundUser;
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+                return null;
+            }
+        }),
     ],
     callbacks: {
         async jwt({token, user}) {
